431ef247b0d3c98fc44ba1a726d19937dc693e99
Naveenkumar247 <wwwvoiceofnaveen3.com>
2026-01-09 15:43:05 +0530

    final certificate route update

--- routes/collegenz.certificate.routes.js
+++ routes/collegenz.certificate.routes.js
@@ -4,13 +4,20 @@ const Certificate = require("../models/Certificate.js");
 const router = express.Router();
 
 router.get("/:code", async (req, res) => {
-  const cert = await Certificate.findOne({ code: req.params.code });
+  try {
+    console.log("Requested code:", req.params.code);
 
-  if (!cert) {
-    return res.status(404).json({ error: "Invalid Certificate" });
+    const cert = await Certificate.findOne({ code: req.params.code });
+
+    if (!cert) {
+      return res.status(404).json({ error: "Invalid Certificate" });
+    }
+
+    res.json(cert);
+  } catch (err) {
+    console.error("Certificate API error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-
-  res.json(cert);
 });
 
 module.exports = router;
